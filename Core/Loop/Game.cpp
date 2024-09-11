344f37ad5357301394d971ee7c3f0f92cd45c0ec
virlanfrancisc <virlangabriel04@gmail.com>
2024-09-11 14:14:30 +0300

    made it run on mac

--- Core/Loop/Game.cpp
+++ Core/Loop/Game.cpp
@@ -24,16 +24,15 @@ Game::~Game() {
 std::atomic<bool> gameIsRunning(true);
 void checkQuitting() {
     SDL_Event event;
-    while (gameIsRunning) {
+    //while (gameIsRunning) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
                 gameIsRunning = false;
-                SDL_Quit();
                 break;
             }
         }
-        SDL_Delay(50);
-    }
+//        SDL_Delay(50);
+//    }
 }
 
 const int FPS = 144;
@@ -55,20 +54,21 @@ void Game::run() {
     l = new NewGameLogic();
 
     Data *d = new Data("FirstMap", 0, "1234");
-    std::thread thread_poll(checkQuitting);
+    //std::thread thread_poll(checkQuitting);
 
     Renderer *r = new Renderer();
     Canvas *c = new Canvas();
     Camera cam = {0, 0, 1280, 720};
 
     while (gameIsRunning) {
+        checkQuitting();
         auto frameStart = std::chrono::steady_clock::now();
         l->processLogic(*d, *c, cam);
         r->drawCanvas(*c, *gc->getRenderer(), cam);
         capFrameRate(frameStart);
     }
-
-    thread_poll.join();
+    SDL_Quit();
+    //thread_poll.join();
     SDL_DestroyWindow(gc->getWindow());
     SDL_DestroyRenderer(gc->getRenderer());
 }
---
+++ cmake-build-debug/CMakeFiles/CppGame.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Core/Context/GameContext.cpp" "CMakeFiles/CppGame.dir/Core/Context/GameContext.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Core/Context/GameContext.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Core/Loop/Game.cpp" "CMakeFiles/CppGame.dir/Core/Loop/Game.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Core/Loop/Game.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Core/main.cpp" "CMakeFiles/CppGame.dir/Core/main.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Core/main.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/GameElements/Entities.cpp" "CMakeFiles/CppGame.dir/GameElements/Entities.cpp.o" "gcc" "CMakeFiles/CppGame.dir/GameElements/Entities.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/GameElements/Entities/Player.cpp" "CMakeFiles/CppGame.dir/GameElements/Entities/Player.cpp.o" "gcc" "CMakeFiles/CppGame.dir/GameElements/Entities/Player.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/GameElements/Layout.cpp" "CMakeFiles/CppGame.dir/GameElements/Layout.cpp.o" "gcc" "CMakeFiles/CppGame.dir/GameElements/Layout.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/GameElements/Map.cpp" "CMakeFiles/CppGame.dir/GameElements/Map.cpp.o" "gcc" "CMakeFiles/CppGame.dir/GameElements/Map.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Logic/Game/NewGameLogic.cpp" "CMakeFiles/CppGame.dir/Logic/Game/NewGameLogic.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Logic/Game/NewGameLogic.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Logic/Menu/MenuLogic.cpp" "CMakeFiles/CppGame.dir/Logic/Menu/MenuLogic.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Logic/Menu/MenuLogic.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Persistance/Save.cpp" "CMakeFiles/CppGame.dir/Persistance/Save.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Persistance/Save.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Persistance/SaveData/Data.cpp" "CMakeFiles/CppGame.dir/Persistance/SaveData/Data.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Persistance/SaveData/Data.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Renderer/Canvas/Canvas.cpp" "CMakeFiles/CppGame.dir/Renderer/Canvas/Canvas.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Renderer/Canvas/Canvas.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Renderer/Canvas/Graphics/TileMap.cpp" "CMakeFiles/CppGame.dir/Renderer/Canvas/Graphics/TileMap.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Renderer/Canvas/Graphics/TileMap.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Renderer/Renderer.cpp" "CMakeFiles/CppGame.dir/Renderer/Renderer.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Renderer/Renderer.cpp.o.d"
+  "/Users/virlanfrancisc/C++/Clion/CppGame/Renderer/TextureManager/TextureManager.cpp" "CMakeFiles/CppGame.dir/Renderer/TextureManager/TextureManager.cpp.o" "gcc" "CMakeFiles/CppGame.dir/Renderer/TextureManager/TextureManager.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
